96eec87ea79b6fc4161b564fcbdb52fd51a92099
vadvoice <shev4enko.vadym@gmail.com>
2020-06-26 18:25:07 +0300

    feature/adaptive-design: just make it more usable for mobile device

--- src/components/Cart/CartItem/CartItem.js
+++ src/components/Cart/CartItem/CartItem.js
@@ -6,13 +6,15 @@ const CartItem = (props) => {
    const { product: { title, imageUrl, description, amount, currency }, cartProducts, actions } = props;
    return (
       <div className="CartItem">
-         <img src={imageUrl} alt={title} />
-         <div className="CartItem__info">
-            <h2>{title}</h2>
-            <p>{description}</p>
-         </div>
-         <div className="CartItem__price">
-            <strong>{`${amount} ${currency}`}</strong>
+         <div className="CartItem__content">
+            <img src={imageUrl} alt={title} />
+            <div className="CartItem__info">
+               <h2>{title}</h2>
+               <p>{description}</p>
+            </div>
+            <div className="CartItem__price">
+               <strong>{`${amount} ${currency}`}</strong>
+            </div>
          </div>
          <div className="CartItem__actions">
             <AddToCart product={props.product} cartProducts={cartProducts} actions={actions} />

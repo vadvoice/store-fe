9d05f466f202954e2ee3f2abb5de5af3fd9341a1
vadvoice <shev4enko.vadym@gmail.com>
2020-08-04 19:59:05 +0300

    general: api base url path

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,11 +1,12 @@
 const detectBaseUrl = () => {
    const hostname = window && window.location && window.location.hostname;
+   const origin = window && window.location && window.location.origin;
    let backendHost = '';
 
    if (~hostname.search('localhost')) {
       backendHost = 'http://localhost:5000/api';
    } else {
-      backendHost = `${process.env.REACT_APP_BACKEND_HOST}/api` || process.env.BASE_URL || 'http://localhost:5000/api';
+      backendHost = `${origin.replace('www', 'www.api') || process.env.REACT_APP_BACKEND_HOST}/api` || process.env.BASE_URL || 'http://localhost:5000/api';
    }
    return backendHost;
 }
